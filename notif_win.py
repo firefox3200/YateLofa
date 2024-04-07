93a84ce2b5fa9a3fd2edb8de95699cc92e98a83e
firefox3200 <141912868+firefox3200@users.noreply.github.com>
2024-04-07 17:07:02 +0500

    add sound

--- notif_win.py
+++ notif_win.py
@@ -1,9 +1,21 @@
 from plyer import notification
 
-# Отправка уведомления
-notification.notify(
-    title='Пример уведомления',
-    message='Это пример уведомления на Windows!',
-    app_name='Моя программа',
-    timeout=10  # Время отображения уведомления в секундах
+def send_notification(title, message, app_name='Моя программа', timeout=10, icon=None, sound=None, actions=None):
+    notification.notify(
+        title=title,
+        message=message,
+        app_name=app_name,
+        timeout=timeout,
+        app_icon=icon,  # Путь к иконке уведомления
+        sound=sound,  # Путь к звуковому файлу для оповещения
+        actions=actions  # Действия при щелчке на уведомлении
+    )
+
+# Пример отправки уведомления с дополнительными параметрами
+send_notification(
+    title='Расширенное уведомление',
+    message='Это расширенное уведомление с иконкой и звуком!',
+    icon='path/to/icon.ico',
+    sound='path/to/sound.wav',
+    actions=['Open', 'Dismiss']
 )
